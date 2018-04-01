e9cd1471bc19392b7baaeec5b58fb969e34b2ba8
erhss <shreejanp@gmail.com>
2018-03-31 21:23:48 -0400

    Update script3.js
    Changed Index1.html to final.html for click "here"

--- scripts/script3.js
+++ scripts/script3.js
@@ -43,7 +43,7 @@ var map = L.map('map', {
 var disclaimer = "The country of Grenada is prone to many natural hazards such as flooding, mass movements and hurricanes. This project aims to identify areas at risk of these hazards due to social vulnerability and physical features of the landscape."
     + "This web map application shows the areas of risk for storm surge and landslides, as well as enumeration districts of different social vulnerability levels. These layers can be toggled on and off."
     + "<p>The web map application, under the Grenada hazard risk assessment project, is mainly to be used as a visualization tool. Generated under specific criteria, this online product should not be viewed as a substitution for government and emergency management protocol. This application is only to be used as a visualization tool or to raise public awareness. As infrastructure and other regional changes take place on the island, the risk areas and levels of risk are likely to change."
-    + "<p>Click <a href ='https://q-schen.github.io/gp481/index1.html' target='_blank'>here</a> to read more about the project. Click outside this box to view the map.";
+    + "<p>Click <a href ='https://q-schen.github.io/gp481/final.html' target='_blank'>here</a> to read more about the project. Click outside this box to view the map.";
 
 
 // splash/info button
